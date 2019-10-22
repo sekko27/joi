657af836698c4fa4a4bd64af44eff6e9cfd4e68a
Dan Levy <dan@danlevy.net>
2019-10-21 19:29:15 -0600

    Adding new RegExp check

--- test/common.js
+++ test/common.js
@@ -22,4 +22,15 @@ describe('Common', () => {
             expect(() => Common.assertOptions()).to.throw('Options must be of type object');
         });
     });
+
+    describe('isRegExp', () => {
+        it('detects RegExp instances', () => {
+            const isRegExp = Common.isRegExp;
+            const pattern = /test/i;
+            const pattern2 = new RegExp('test', 'i');
+            expect(isRegExp(pattern)).to.be.true;
+            expect(isRegExp(pattern2)).to.be.true;
+            expect(isRegExp('🤓')).to.be.false;
+        });
+    });
 });
